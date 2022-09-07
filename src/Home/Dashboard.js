0dedf36a4f8d5068a5ba12d5a41212069eb00ad5
Thrinkxs <emmanuelozigue@gmail.com>
2022-09-07 17:42:28 +0200

    'le-walk'

---
+++ src/Home/Dashboard.js
@@ -0,0 +1,36 @@
+import lewalk from '../../assets/lewalk.png'
+import logo from '../../assets/logo.png'
+import ASD from '../../assets/ASD.png'
+import me from '../../assets/me.jpg' 
+
+const dashData = [
+    {
+        name: "Ingwe Health",
+        category: "Health",
+        numberCompany: "25",
+        image:lewalk.png,
+        bgColor: 'red'
+    },
+    {
+        name: "Luziman",
+        category: "Automobile",
+        numberCompany: "30",
+        image:logo,
+        bgColor: 'yellow'
+    },
+    {
+        name: "ASTUDENTSDREAM",
+        category: "Education",
+        numberCompany: "60",
+        image:ASD,
+        bgColor: 'blue'
+    },
+    {
+        name: "Emmanuel",
+        category: "Medical Doctor",
+        numberCompany: "35",
+        image:me,
+        bgColor: 'torquoise'
+    },
+]
+export default dashData;
---
+++ src/Home/Home.js
@@ -0,0 +1,66 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text,Dimensions, StyleSheet, SafeAreaView,Image, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import dashData from './Dashboard';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height; 
+
+// create a component
+const Home = ({navigation}) => {
+    return (
+        <SafeAreaView>
+            <FlatList
+            data={dashData}
+            contentInset={{padding:12}}
+            renderItem={({item})=>{
+                return(
+                    <TouchableOpacity style={{margin:20, height:windowHeight * 0.2, backgroundColor:item.bgColor, borderRadius:5}}>
+                      <View style={{backgroundColor:item.bgColor}}>
+                      <Text styele={styles.category} >
+                            {item.category}
+                        </Text>
+                        <Text style={styles.name}>
+                            {item.name}
+                        </Text>
+                        <Text>
+                            {item.numberCompany}
+                        </Text>
+                        
+                      </View>
+                      <Image 
+                      style={{
+                        height:90, width:60, position:'absolute', right:0, margin:10, bottom:0
+                      }}source={item.image}/>
+                        
+                    </TouchableOpacity>
+                )
+            }}
+            
+            >
+
+            </FlatList>
+        </SafeAreaView>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    category:{
+fontSize: 24,
+fontWeight: '200'
+    },
+    name:{
+fontSize:18
+    },
+});
+
+//make this component available to the app
+export default Home;
